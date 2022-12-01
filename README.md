96567a6f1a2b1366cc882e46200a293316240927
root <toufiqelahy@hotmail.com>
2022-12-01 10:45:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+# genesis-contracts
+
+#### Setup genesis
+
+Setup genesis whenever contracts get changed
+### 1. Install dependencies and submodules
+```bash
+$ npm install
+$ git submodule init
+$ git submodule update
+```
+
+### 2. Compile Matic contracts
+```bash
+$ cd matic-contracts
+$ npm install
+$ node scripts/process-templates.js --bor-chain-id <bor-chain-id>
+$ npm run truffle:compile
+$ cd ..
+```
+
+### 3. Generate Bor validator set sol file
+
+Following command will generate `BorValidatorSet.sol` file from `BorValidatorSet.template` file.
+
+```bash
+# Generate bor validator set using stake and balance
+# Modify validators.json before as per your need
+$ node generate-borvalidatorset.js --bor-chain-id <bor-chain-id> --heimdall-chain-id <heimdall-chain-id>
+```
+
+### 4. Compile contracts
+```bash
+$ npm run truffle:compile
+```
+
+### 5. Generate genesis file
+
+Following command will generate `genesis.json` file from `genesis-template.json` file.
+
+```bash
+# Generate genesis file
+$ node generate-genesis.js --bor-chain-id <bor-chain-id> --heimdall-chain-id <heimdall-chain-id>
+```
+
+### 6. Run Tests
+```bash
+$ npm run testrpc
+$ npm test
+```
---
+++ generate-genesis.js
@@ -0,0 +1,110 @@
+const { spawn } = require("child_process")
+const program = require("commander")
+const nunjucks = require("nunjucks")
+const fs = require("fs")
+const web3 = require("web3")
+
+const validators = require("./validators")
+
+// load and execute bor validator set
+require("./generate-borvalidatorset")
+
+program.version("0.0.1")
+program.option("-c, --bor-chain-id <bor-chain-id>", "Bor chain id", "15001")
+program.option(
+  "-o, --output <output-file>",
+  "Genesis json file",
+  "./genesis.json"
+)
+program.option(
+  "-t, --template <template>",
+  "Genesis template json",
+  "./genesis-template.json"
+)
+program.parse(process.argv)
+
+// compile contract
+function compileContract(key, contractFile, contractName) {
+  return new Promise((resolve, reject) => {
+    const ls = spawn("solc", [
+      "--bin-runtime",
+      "openzeppelin-solidity/=node_modules/openzeppelin-solidity/",
+      "solidity-rlp/=node_modules/solidity-rlp/",
+      "/=/",
+      // "--optimize",
+      // "--optimize-runs",
+      // "200",
+      contractFile
+    ])
+
+    const result = []
+    ls.stdout.on("data", data => {
+      result.push(data.toString())
+    })
+
+    ls.stderr.on("data", data => {
+      result.push(data.toString())
+    })
+
+    ls.on("close", code => {
+      console.log(`child process exited with code ${code}`)
+      const fn = code === 0 ? resolve : reject
+      fn(result.join(""))
+    })
+  }).then(compiledData => {
+    compiledData = compiledData.replace(
+      new RegExp(`======= ${contractFile}:${contractName} =======\nBinary of the runtime part:` + '[ ]?'),
+      "@@@@"
+    )
+
+    const matched = compiledData.match(/@@@@\n([a-f0-9]+)/)
+    return { key, compiledData: matched[1], contractName, contractFile }
+  })
+}
+
+// compile files
+Promise.all([
+  compileContract(
+    "borValidatorSetContract",
+    "contracts/BorValidatorSet.sol",
+    "BorValidatorSet"
+  ),
+  compileContract(
+    "borStateReceiverContract",
+    "contracts/StateReceiver.sol",
+    "StateReceiver"
+  ),
+  compileContract(
+    "maticChildERC20Contract",
+    "matic-contracts/contracts/child/MRC20.sol",
+    "MRC20"
+  )
+]).then(result => {
+  const totalMaticSupply = web3.utils.toBN("1000000000")
+
+  var validatorsBalance = web3.utils.toBN(0)
+  validators.forEach(v => {
+    validatorsBalance = validatorsBalance.add(web3.utils.toBN(v.balance))
+    v.balance = web3.utils.toHex(web3.utils.toWei(String(v.balance)))
+  })
+
+  const contractBalance = totalMaticSupply.sub(validatorsBalance)
+  const data = {
+    chainId: program.borChainId,
+    validators: validators,
+    maticChildERC20ContractBalance: web3.utils.toHex(
+      web3.utils.toWei(contractBalance.toString())
+    )
+  }
+
+  result.forEach(r => {
+    data[r.key] = r.compiledData
+  })
+
+  const templateString = fs.readFileSync(program.template).toString()
+  const resultString = nunjucks.renderString(templateString, data)
+  fs.writeFileSync(program.output, resultString)
+}).catch(err => {
+  console.log(err)
+  process.exit(1)
+})
